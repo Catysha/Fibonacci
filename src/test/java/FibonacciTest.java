cc700de8aaa296c5d038e5173df844bdffff4fb3
Catysha <viktorekaterina2005@gmail.com>
2025-01-13 12:46:10 +0300

    Add changes

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -23,12 +23,6 @@ public class FibonacciTest {
     }
 
     @Test
-    public void testGetFibonacci_Three() {
-        List<Integer> result = Fibonacci.getFibonacci(3);
-        assertEquals(List.of(0, 1, 1), result, "Список должен содержать 0, 1 и 1 для n = 3");
-    }
-
-    @Test
     public void testGetFibonacci_Ten() {
         List<Integer> result = Fibonacci.getFibonacci(10);
         assertEquals(List.of(0, 1, 1, 2, 3, 5, 8, 13, 21, 34), result, "Список должен содержать первые 10 чисел Фибоначчи");
